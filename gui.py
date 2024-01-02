dc57ae376387264d4894e716b081f7dde1bdba80
Jurgen Stegeman <jurgenstegeman@live.nl>
2024-01-02 19:54:08 +0100

    set gui title and size

--- gui.py
+++ gui.py
@@ -4,3 +4,5 @@ import tkinter as tk
 class WatermarkingDesktopApp:
     def __init__(self, master):
         self.master = master
+        self.master.title("Watermarking GUI")
+        self.master.geometry("600x400")
