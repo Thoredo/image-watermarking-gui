487ef5871ec0051712e77ee06bbaec26bd920812
Jurgen Stegeman <jurgenstegeman@live.nl>
2024-01-03 12:37:37 +0100

    Add main menu

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import filedialog
 
 
 class WatermarkingDesktopApp:
@@ -6,3 +7,14 @@ class WatermarkingDesktopApp:
         self.master = master
         self.master.title("Watermarking GUI")
         self.master.geometry("600x400")
+        self.create_main_menu()
+
+    def create_main_menu(self):
+        self.select_img_label = tk.Label(text="Please select an image to edit")
+        self.select_img_label.grid(row=0, column=0, padx=230, pady=(170, 10))
+
+        self.browse_button = tk.Button(text="Browse", command=self.browse_image)
+        self.browse_button.grid(row=1, column=0)
+
+    def browse_image(self):
+        image = filedialog.askopenfile()
