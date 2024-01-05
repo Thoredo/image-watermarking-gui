6edc4db1ebdd2a49fe142171eaf3e933c6025055
Jurgen Stegeman <jurgenstegeman@live.nl>
2024-01-05 14:28:22 +0100

    Add image size label

--- gui.py
+++ gui.py
@@ -9,13 +9,27 @@ class WatermarkingDesktopApp:
         self.master.title("Watermarking GUI")
         self.master.config(padx=20, pady=20, bg="black")
         self.master.minsize(height=100, width=500)
-        self.create_blank_photo()
+        self.height_main = 0
+        self.width_main = 0
 
-        self.tk_image = None
+        self.create_main_menu()
 
-    def create_blank_photo(self):
+    def create_main_menu(self):
+        self.blank_photo()
+        self.image_size_label()
+
+    def blank_photo(self):
         self.blank_photo = Image.new(mode="RGBA", size=(700, 600), color="#242424")
         self.image1 = ImageTk.PhotoImage(self.blank_photo)
         self.panel = tk.Label(self.master, image=self.image1)
         self.panel.image = self.image1
         self.panel.grid(column=0, rowspan=15)
+
+    def image_size_label(self):
+        self.image_size = tk.Label(
+            text=f"Image size {self.height_main}/{self.width_main} (height/width)",
+            bg="#000000",
+            fg="#fafafa",
+            font=("Arial", 8),
+        )
+        self.image_size.grid(column=0, row=16)
