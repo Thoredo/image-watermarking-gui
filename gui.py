b0220978361fdf65960fa9a29412f0bc57a0958a
Jurgen Stegeman <jurgenstegeman@live.nl>
2024-01-07 14:17:14 +0100

    Move font type widget to own file and class

--- gui.py
+++ gui.py
@@ -16,6 +16,7 @@ from widgets.rotation_buttons import RotationButtons
 from widgets.color_widget import ColorWidget
 from widgets.opacity_widget import OpacityWidget
 from widgets.font_size_widget import FontSizeWidget
+from widgets.font_type_widget import FontTypeWidget
 
 
 class WatermarkingDesktopApp:
@@ -31,7 +32,6 @@ class WatermarkingDesktopApp:
         self.image_frame = ImageFrame(self.master)
         self.size_label = ImageSizeLabel(self.master)
         self.select_file = SelectFile(self.master, self.image_frame, self.size_label)
-        self.select_font_widget()
         self.save_widget()
         self.watermark = Watermark(
             self.master,
@@ -46,47 +46,13 @@ class WatermarkingDesktopApp:
         self.color_widget = ColorWidget(self.master, self.watermark)
         self.opacity_widget = OpacityWidget(self.master, self.watermark)
         self.font_size_widget = FontSizeWidget(self.master, self.watermark)
-
-    def select_font_widget(self):
-        font_list = font_manager.findSystemFonts(fontpaths=None, fontext="ttf")
-        final_font_list = []
-        formatted_font_list = [font.split("\\")[-1] for font in font_list]
-        for font in formatted_font_list:
-            if ".otf" not in font:
-                final_font_list.append(font)
-        final_font_list.sort()
-        # Find the index of 'arial.ttf' in the font_list
-        arial_index = final_font_list.index("arial.ttf")
-
-        # Trim the font list up to 'arial.ttf' (inclusive)
-        trimmed_font_list = final_font_list[arial_index:]
-
-        default_font = tk.StringVar(self.master)
-        default_font.set("arial")
-
-        self.font_type_label = tk.Label(
-            text="Font:", bg="#000000", fg="#fafafa", font=("Arial", 12, "bold")
-        )
-        self.font_type_label.grid(column=4, row=12, sticky=tk.W)
-
-        self.chosen_font_type = Combobox(
-            self.master,
-            textvariable=default_font,
-            values=trimmed_font_list,
-            state="readonly",
-        )
-        self.chosen_font_type.grid(column=5, row=12, sticky=tk.E)
-        self.chosen_font_type.bind("<<ComboboxSelected>>", self.change_font_type)
+        self.select_font_widget = FontTypeWidget(self.master, self.watermark)
 
     def save_widget(self):
         self.save_button = tk.Button(
             text="Save", bg="#e7e7e7", fg="black", command=self.save_image
         )
         self.save_button.grid(column=7, row=16)
-
-    def change_font_type(self, event=None):
-        self.watermark.font_main = self.chosen_font_type.get()
-        self.watermark.show_watermark()
 
     def save_image(self):
         path = filedialog.asksaveasfilename(
---
+++ widgets/font_type_widget.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from tkinter.ttk import Combobox
+from matplotlib import font_manager
+
+
+class FontTypeWidget:
+    def __init__(self, master, watermark):
+        self.master = master
+        self.watermark = watermark
+
+        font_list = font_manager.findSystemFonts(fontpaths=None, fontext="ttf")
+        final_font_list = []
+        formatted_font_list = [font.split("\\")[-1] for font in font_list]
+        for font in formatted_font_list:
+            if ".otf" not in font:
+                final_font_list.append(font)
+        final_font_list.sort()
+        # Find the index of 'arial.ttf' in the font_list
+        arial_index = final_font_list.index("arial.ttf")
+
+        # Trim the font list up to 'arial.ttf' (inclusive)
+        trimmed_font_list = final_font_list[arial_index:]
+
+        default_font = tk.StringVar(self.master)
+        default_font.set("arial")
+
+        self.font_type_label = tk.Label(
+            text="Font:", bg="#000000", fg="#fafafa", font=("Arial", 12, "bold")
+        )
+        self.font_type_label.grid(column=4, row=12, sticky=tk.W)
+
+        self.chosen_font_type = Combobox(
+            self.master,
+            textvariable=default_font,
+            values=trimmed_font_list,
+            state="readonly",
+        )
+        self.chosen_font_type.grid(column=5, row=12, sticky=tk.E)
+        self.chosen_font_type.bind("<<ComboboxSelected>>", self.change_font_type)
+
+    def change_font_type(self, event=None):
+        self.watermark.font_main = self.chosen_font_type.get()
+        self.watermark.show_watermark()
